b82624705766b4907bf2150dc32dde182b79379c
Svyatoslav Delukin <tarzeron@gmail.com>
2018-12-11 17:04:56 +0200

    Chore (GSR, safe math)
    gsr stubs
    safe math

--- contracts/GSR.sol
+++ contracts/GSR.sol
@@ -1,9 +1,106 @@
 pragma solidity ^0.4.24;
 
 import "./Ownable.sol";
+import "./GEO.sol";
+import "../math/SafeMath8.sol";
+import "../math/SafeMath.sol";
+import "../math/SafeMath16.sol";
 
 contract GSR is Ownable {
+    using SafeMath8 for uint8;
+    using SafeMath16 for uint16;
     using SafeMath for uint256;
 
+    GEO public geo;
+
+    // (registry name) => (epoch) => (address) => (vote token amount)
+    mapping (string => mapping(uint16 => mapping(address => uint256))) private registry;
+    mapping (address => uint256) public stake;
+
+    uint16 public currentEpoch = 0;
+
+
+    function addRegistry(string _name)
+    onlyOwner
+    public
+    {
+
+    }
+
+    function removeRegistry(string _name)
+    onlyOwner
+    public
+    {
+
+    }
+
+    function vote(string _registryName)
+    public
+    {
+
+    }
+
+    function cancelVote(string _registryName)
+    public
+    {
+
+    }
+
+    function voteService(uint256 _amount)
+    public
+    {
+
+    }
+
+    function voteServiceLockup(uint256 _amount)
+    public
+    {
+
+    }
+
+    function withdraw()
+    public
+    {
+
+    }
+
+    function withdraw()
+    public
+    {
+
+    }
+
+    /**
+    * @dev Check and change number of current epoch
+    */
+    function checkEpoch()
+    private
+    returns(uint16)
+    {
+        if (epochTime < now) {
+            return increaseEpoch();
+        }
+    }
+
+    /**
+    * @dev Increase current epoch
+    */
+    function increaseEpoch()
+    private
+    returns(uint16)
+    {
+        restartEpochTime();
+        currentEpoch = uint16(currentEpoch.add(1));
+        return currentEpoch;
+    }
+
+    /**
+    * @dev Start time from now
+    */
+    function restartEpochTime()
+    private
+    {
+        epochTime = now.add(epochTimeLimit);
+    }
 
 }
--- contracts/math/SafeMath8.sol
+++ contracts/math/SafeMath8.sol
@@ -1,14 +1,16 @@
 pragma solidity ^0.4.24;
 
+
 /**
  * @title SafeMath
  * @dev Math operations with safety checks that revert on error
  */
-library SafeMath {
+library SafeMath8 {
+
     /**
     * @dev Multiplies two numbers, reverts on overflow.
     */
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    function mul(uint8 a, uint8 b) internal pure returns (uint8) {
         // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
         // benefit is lost if 'b' is also tested.
         // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
@@ -16,7 +18,7 @@ library SafeMath {
             return 0;
         }
 
-        uint256 c = a * b;
+        uint8 c = a * b;
         require(c / a == b);
 
         return c;
@@ -25,10 +27,9 @@ library SafeMath {
     /**
     * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
     */
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // Solidity only automatically asserts when dividing by 0
-        require(b > 0);
-        uint256 c = a / b;
+    function div(uint8 a, uint8 b) internal pure returns (uint8) {
+        require(b > 0); // Solidity only automatically asserts when dividing by 0
+        uint8 c = a / b;
         // assert(a == b * c + a % b); // There is no case in which this doesn't hold
 
         return c;
@@ -37,18 +38,17 @@ library SafeMath {
     /**
     * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
     */
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    function sub(uint8 a, uint8 b) internal pure returns (uint8) {
         require(b <= a);
-        uint256 c = a - b;
-
+        uint8 c = a - b;
         return c;
     }
 
     /**
     * @dev Adds two numbers, reverts on overflow.
     */
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
+    function add(uint8 a, uint8 b) internal pure returns (uint8) {
+        uint8 c = a + b;
         require(c >= a);
 
         return c;
@@ -58,7 +58,7 @@ library SafeMath {
     * @dev Divides two numbers and returns the remainder (unsigned integer modulo),
     * reverts when dividing by zero.
     */
-    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    function mod(uint8 a, uint8 b) internal pure returns (uint8) {
         require(b != 0);
         return a % b;
     }
