292482b1623b7aa9a99e3de80b05c9dddc1cebd2
Svyatoslav Delukin <tarzeron@gmail.com>
2019-01-04 13:59:39 +0200

    Feat (wrapper gsr) stubs for wrapper

--- wrapper/geo_service_registry.py
+++ wrapper/geo_service_registry.py
@@ -18,3 +18,27 @@ class GeoServiceRegistry:
 
     def is_registry_exist(self, registry_name):
         return self.contract.functions.isRegistryExist(registry_name).call()
+
+    def withdraw(amount):
+        pass
+
+    def vote_service_lockup_for_new_registry(registry_name):
+        pass
+
+    def vote_service_lockup(registry_name, _candidates, _amounts):
+        pass
+
+    def vote_service_for_new_registry(registry_name):
+        pass
+
+    def vote_service(registry_name, _candidates, _amounts):
+        pass
+
+    def sum_of_array(array):
+        pass
+
+    def set_vote_weight_in_lockup_period(new_amount):
+        pass
+
+    def make_deposit(addition_amount):
+        pass
