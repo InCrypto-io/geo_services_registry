56b7d461e98325f1a50d1f837e8f8aca31aaa9b0
Svyatoslav Delukin <tarzeron@gmail.com>
2019-01-04 15:24:04 +0200

    Chore(wrapper geo token) add wrapper

---
+++ wrapper/geo_oken.py
@@ -0,0 +1,77 @@
+import json
+
+
+class GEOToken:
+
+    def __init__(self, connection, address):
+        interface_file = open("./build/contracts/GEOToken.json", "r")
+        contract_interface = json.load(interface_file)
+        interface_file.close()
+
+        w3 = connection.get_web3()
+
+        self.contract = w3.eth.contract(
+            address=address,
+            abi=contract_interface['abi'],
+        )
+
+        if (len(connection.get_accounts()) > 0):
+            self.account = connection.get_accounts()[0]
+        else:
+            self.account = ""
+
+    def set_account(self, account):
+        self.account = account
+
+    def transfer_from(self, sender, receiver, value):
+        self.contract.functions.transferFrom(sender, receiver, value) \
+            .transact({'from': self.account, 'gas': 100000})
+
+    def transfer(self, receiver, value):
+        self.contract.functions.transfer(receiver, value) \
+            .transact({'from': self.account, 'gas': 100000})
+
+    def total_supply(self):
+        return self.contract.functions.totalSupply().call()
+
+    def set_individual_lockup_expire_time(self, who, time):
+        self.contract.functions.setIndividualLockupExpireTime(who, time) \
+            .transact({'from': self.account, 'gas': 100000})
+
+    def deny_transfer_in_lockup_period(self, who):
+        self.contract.functions.denyTransferInLockupPeriod(who) \
+            .transact({'from': self.account, 'gas': 100000})
+
+    def allow_transfer_in_lockup_period(self, who):
+        self.contract.functions.allowTransferInLockupPeriod(who) \
+            .transact({'from': self.account, 'gas': 100000})
+
+    def is_lockup_expired(self, _who):
+        return self.contract.functions.isLockupExpired(_who).call()
+
+    def increase_allowance(self, spender, added_value):
+        self.contract.functions.spender(spender, added_value) \
+            .transact({'from': self.account, 'gas': 100000})
+
+    def decrease_allowance(self, spender, subtracted_value):
+        self.contract.functions.spender(spender, subtracted_value) \
+            .transact({'from': self.account, 'gas': 100000})
+
+    def burn(self, account, value):
+        self.contract.functions.burn(account, value) \
+            .transact({'from': self.account, 'gas': 100000})
+
+    def mint(self, account, value):
+        self.contract.functions.mint(account, value) \
+            .transact({'from': self.account, 'gas': 100000})
+
+    def balance_of(self, owner):
+        return self.contract.functions.balanceOf(owner).call()
+
+    def approve(self, spender, value):
+        self.contract.functions.approve(spender, value) \
+            .transact({'from': self.account, 'gas': 100000})
+
+    def allowance(self, owner, spender):
+        self.contract.functions.allowance(owner, spender) \
+            .transact({'from': self.account, 'gas': 100000})
--- wrapper/geo_service_registry.py
+++ wrapper/geo_service_registry.py
@@ -1,5 +1,4 @@
 import json
-from web3 import Web3
 
 
 class GeoServiceRegistry:
@@ -28,29 +27,29 @@ class GeoServiceRegistry:
         return self.contract.functions.isRegistryExist(registry_name).call()
 
     def withdraw(self, amount):
-        self.contract.functions.withdraw(amount)\
+        self.contract.functions.withdraw(amount) \
             .transact({'from': self.account, 'gas': 100000})
 
     def vote_service_for_new_registry(self, registry_name):
-        self.contract.functions.voteServiceForNewRegistry(registry_name)\
+        self.contract.functions.voteServiceForNewRegistry(registry_name) \
             .transact({'from': self.account, 'gas': 100000})
 
     def vote_service_lockup_for_new_registry(self, registry_name):
-        self.contract.functions.voteServiceLockupForNewRegistry(registry_name)\
+        self.contract.functions.voteServiceLockupForNewRegistry(registry_name) \
             .transact({'from': self.account, 'gas': 100000})
 
     def vote_service(self, registry_name, _candidates, _amounts):
-        self.contract.functions.voteService(registry_name, _candidates, _amounts)\
+        self.contract.functions.voteService(registry_name, _candidates, _amounts) \
             .transact({'from': self.account, 'gas': 100000})
 
     def vote_service_lockup(self, registry_name, _candidates, _amounts):
-        self.contract.functions.voteServiceLockup(registry_name, _candidates, _amounts)\
+        self.contract.functions.voteServiceLockup(registry_name, _candidates, _amounts) \
             .transact({'from': self.account, 'gas': 100000})
 
     def set_vote_weight_in_lockup_period(self, new_amount):
-        self.contract.functions.setVoteWeightInLockupPeriod(new_amount)\
+        self.contract.functions.setVoteWeightInLockupPeriod(new_amount) \
             .transact({'from': self.account, 'gas': 100000})
 
     def make_deposit(self, addition_amount):
-        self.contract.functions.makeDeposit(addition_amount)\
+        self.contract.functions.makeDeposit(addition_amount) \
             .transact({'from': self.account, 'gas': 100000})
