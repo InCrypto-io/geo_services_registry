50fb9a1eb3951b545adca9e45c85bf1f2c47ea0c
Svyatoslav Delukin <tarzeron@gmail.com>
2019-01-09 16:38:18 +0200

    Feat(events cache) stubs

---
+++ wrapper/events_cacher.py
@@ -0,0 +1,22 @@
+class EventCacher:
+    def __init__(self, connection, gsr_address):
+        self.connection = connection
+        self.gsr_address = gsr_address
+
+    def collect(self):
+        pass
+
+    def collect(self):
+        pass
+
+    def erase_all(self, from_block_number=0):
+        pass
+
+    def get_events_count(self):
+        pass
+
+    def get_event(self, index):
+        pass
+
+    def get_events_in_range(self, block_start, block_end):
+        pass
