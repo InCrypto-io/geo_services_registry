c56adde24e575346936cc034c88b9728d1f0ef2d
Svyatoslav Delukin <tarzeron@gmail.com>
2019-01-17 09:55:44 +0200

    Chore(REST) REST API stubs

--- wrapper/rest.py
+++ wrapper/rest.py
@@ -35,8 +35,26 @@ class REST:
         self.allow_process_events = False
 
     async def handle(self, request):
-        text = "/" + request.match_info.get('request')
+        text = "/" + request.match_info.get('request') + "\n" + str(request.rel_url.query)
         return web.Response(text=text)
+
+    async def get_current_block_number(self, request):
+        pass
+
+    async def erase(self, request):
+        pass
+
+    async def get_registries(self, request):
+        pass
+
+    async def is_registry_exist(self, request):
+        pass
+
+    async def get_votes_list(self, request):
+        pass
+
+    async def get_vote_for_candidate(self, request):
+        pass
 
     def process_events(self):
         while self.allow_process_events:
@@ -47,6 +65,12 @@ class REST:
     def launch(self):
         app = web.Application()
         app.add_routes([web.get('/', self.handle),
+                        web.get('/blocks/currentBlock', self.get_current_block_number),
+                        web.get('/blocks/erase', self.erase),
+                        web.get('/registries/list', self.get_registries),
+                        web.get('/registries/exist', self.is_registry_exist),
+                        web.get('/votes/list', self.is_registry_exist),
+                        web.get('/votes/candidate', self.get_vote_for_candidate),
                         web.get('/{request}', self.handle)])
 
         self.allow_process_events = True
